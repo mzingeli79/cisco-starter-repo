7e1b2cbebb051c8b0f8867cf6f1046bb7b2c94c6
mzingeli79 <mzingeli79@gmail.com>
2022-07-10 20:05:04 +0200

    My second commit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,26 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>I am Ndumi</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Banner/>
+        <Exhibit/>
     </div>
   );
-}
+};
+
+class Banner extends React.Component {
+  render(){
+    return (
+    <div>
+      <header className='banner'>
+        <section className='banner-name'>Sextant</section>
+      </header>
+    </div>
+    );
+  }
+};
+
+class Exhibit extends React.Component {
+  render(){
+    return (
+      <div>
+        <DataPoints/>
+      </div>
+    );
+  }
+};
+
+class DataPoints extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className='exhibit-class'>
+        <h1>Data Points</h1>
+        <section className='display-components'>
+          <div className='display'>
+            <UsersPublicIP/>
+          </div>
+          <div className='display'>
+            <LatencyInformation/>
+          </div>
+        </section>
+      </div>
+    );
+  }
+};
+
+class UsersPublicIP extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      userPublicIP: ''
+    };
+  }
+  render(){
+    return (
+      <div>
+        <section>
+          <h1>User-Public-IP:</h1>
+          <p className='user-ip'>{this.state.userPublicIP='123.456.789'}</p>
+        </section>
+      </div>
+    );
+  }
+};
+
+class LatencyInformation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latencyInfo: ''
+    };
+  }
+  render(){
+    return (
+      <div>
+        <section>
+          <h1>Latency-Information:</h1>
+          <p className='latency-info'>{this.state.latencyInfo='11.2[ms]'}</p>
+        </section>
+      </div>
+    );
+  }
+};
 
 export default App;
